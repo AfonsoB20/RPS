160dfb71f4dbdcf4ced30cf6cb7d7737bd752422
AfonsoB20 <afonsobastos01@gmail.com>
2023-02-27 01:12:19 +0000

    working

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
-class js {
-    getComputerChoice(){
-    let x = Math.random()*2;
+
+function getComputerChoice(){
+    let x = Math.floor(Math.random()*3);
     if(x == 0 ){
         return "Rock";
     } else if(x == 1){
@@ -8,9 +8,27 @@ class js {
     } else if(x == 2){
         return "Scissors";
     }
-
-    return " ";
-    }
-    getComputerChoice();
     
+    return " ";
+    
+    
+}
+
+function oneRound(playerSelection, computerSelection){
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("Rock Paper Scissors");
+   // console.log(computerSelection)
+    if(playerSelection == "Rock" && computerSelection == "Scissors"){
+        return "You Won!"
+    } else if(playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You Lost"
+    }
+    return "Draw";
+}
+
+function game(){
+    for(let i=0; i<5; i++){
+        console.log(oneRound());
+    }
+
 }
